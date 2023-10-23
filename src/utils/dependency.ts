09f4de67508e3abd9b76e109d0f423f7f07a5835
申君健 <40288193@qq.com>
2023-10-23 17:06:57 +0800

    fix: load CDN url (#28)

--- src/utils/dependency.ts
+++ src/utils/dependency.ts
@@ -12,7 +12,7 @@ export interface Dependency {
 }
 
 export type Cdn = 'unpkg' | 'jsdelivr' | 'jsdelivr-fastly'
-export const cdn = useLocalStorage('setting-cdn', 'https://unpkg.com')
+export const cdn = useLocalStorage('setting-cdn', localStorage.getItem('setting-cdn') || 'https://unpkg.com')
 
 export function genCdnLink(pkg: string, version: string | undefined, path: string) {
   version = version ? `@${version}` : ''
